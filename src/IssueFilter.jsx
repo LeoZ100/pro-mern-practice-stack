abce05538c993f5b94553ccfb6d57d39f6e60f28
Alan Arvelo <alanarvelo@gmail.com>
2017-06-19 17:31:53 +0200

    HMR

---
+++ src/IssueFilter.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+import React from 'react';
+
+export default class IssueFilter extends React.Component{
+	render() {
+		return (
+			<div> This wataa placerhlr the issue filter. </div>	
+		);
+	}
+}
